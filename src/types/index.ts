d1adf1ada10b908ec3c2edf6915a4e2e4c8ad32c
p.zabolotny <p.zabolotny@bi.zone>
2024-03-30 02:03:19 +0300

    Sprint 2 | Request class

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,10 @@
 export type TProps = Record<string, any>;
+
+export type TOptions = {
+  method?: string;
+  headers?: Record<string, string>;
+  data?: unknown;
+  timeout?: number;
+};
+
+export type TRequest = (url: string, options?: TOptions) => Promise<unknown>
